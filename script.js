03a98d07fe7f11842b816ffa4558644f82507139
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-07-31 13:42:25 -0300

    main

--- script.js
+++ script.js
@@ -106,9 +106,9 @@ function mostrarProgramacao(button, data) {
       <ul>
         <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
         <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-        <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-        <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E07-T2</span></li> <hr class="filme-separador">
-        <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">INVASÃO SECRETA - E04-T1</span></li> <hr class="filme-separador">
+        <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+        <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E07-T2</span></li> <hr class="filme-separador">
+        <li><span class="horario">19:35</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">INVASÃO SECRETA - E04-T1</span></li> <hr class="filme-separador">
       </ul>
     `;
   } else if (data === '10/08') {
