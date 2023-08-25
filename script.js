f3c0834ffe3d62f802ad0eedbdb51dc2032f6f2e
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-08-25 11:15:29 -0300

    main

--- script.js
+++ script.js
@@ -101,7 +101,25 @@ function mostrarProgramacao(button, data) {
     <li><span class="horario">22:25</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">PARAÍSO</span></li> <hr class="filme-separador">    
     </ul>
   `;         
+} else if (data === '02/09') {
+  programacaoDiv.innerHTML = `
+    <ul>
+    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+    <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E08-T3</span></li> <hr class="filme-separador">
+    <li><span class="horario">13:50</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">(DES)ENCANTO - E02-T5</span></li> <hr class="filme-separador">    
+    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+    </ul>
+  `;
 
+  // Definir próximo filme  
+} else if (data === '03/09') {
+  programacaoDiv.innerHTML = `
+    <ul>
+    <li><span class="horario">19:20</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E09-T3</span></li> <hr class="filme-separador">
+    <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">(DES)ENCANTO - E03-T5</span></li> <hr class="filme-separador">    
+    <li><span class="horario">20:10</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
+    </ul>
+  `;     
 } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
