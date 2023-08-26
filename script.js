22db6098e2ad681271e338265ddf48928ffde507
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-08-26 11:08:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,20 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
   
- if (data === '25/08') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E23-T2</span></li> <hr class="filme-separador">
-    <li><span class="horario">20:00</span> <span class="label-dub">LEG</span> <span class="class-14">14</span> <span class="filme">RAGNAROK - E02-T3</span></li> <hr class="filme-separador">    
-    <li><span class="horario">20:55</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">IMPÉRIO DA DOR - E03-T1</span></li> <hr class="filme-separador">
-    <li><span class="horario">22:25</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">AGENTE STONE</span></li> <hr class="filme-separador">
-    </ul>
-  `;
-// Excluir primeiro item Em Breve  
-} else if (data === '26/08') {
+ if (data === '26/08') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
@@ -125,15 +112,6 @@ function mostrarProgramacao(button, data) {
     programacaoDiv.innerHTML = `    
       <ul>
 
-        <li class="filme-poster">
-        <img src="imgs//poster-guardioes-da-galaxia3.jpg" alt="Guardiões da Galáxia - Vol. 3">
-        <div class="filme-texto">
-          <span class="dia">26/08</span>
-          <span class="class-14">14</span>
-          <span class="filme">GUARDIÕES DA GALÁXIA - VOL. 3</span>
-        </div>
-      </li>
-      <hr class="filme-separador">
 
       <li><span class="dia">30/08</span> <span class="class-14">14</span> <span class="filme">UNTOLD: A VERGONHA DO DOPING</span></li>      
       <hr class="filme-separador">
