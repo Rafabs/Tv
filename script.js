605dfb3eb07ada13b424e175dec88cdf73b66636
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-09-01 09:13:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,19 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
  
- if (data === '31/08') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">15:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E06-T3</span></li> <hr class="filme-separador">
-    <li><span class="horario">16:00</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">THE FLASH (2023)</span></li> <hr class="filme-separador">    
-    <li><span class="horario">18:40</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">COPA SULAMERICANA</span></li> <hr class="filme-separador">
-    </ul>
-  `;    
-  // Excluir primeiro e segundo item Em Breve  
-  // Aguardar estréia de Desencanto para saber o tempo do episódio para inserir o horário de I AM GROOT  06/09
-} else if (data === '01/09') {
+ if (data === '01/09') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
@@ -276,26 +264,6 @@ function mostrarProgramacao(button, data) {
 } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
-
-        <li class="filme-poster">
-        <img src="imgs//poster-paraiso.jpg" alt="Paraiso">
-        <div class="filme-texto">
-          <span class="dia">01/09</span>
-          <span class="class-16">16</span>
-          <span class="filme">PARAÍSO</span>
-        </div>
-      </li>
-      <hr class="filme-separador">
-
-        <li class="filme-poster">
-          <img src="imgs//poster-desencanto.jpg" alt="(Des)encanto">
-          <div class="filme-texto">
-          <span class="dia">01/09</span>
-            <span class="class-14">14</span>
-            <span class="filme">(DES)ENCANTO - T5</span>
-          </div>
-        </li>
-        <hr class="filme-separador">
 
         <li class="filme-poster">
           <img src="imgs//poster-groot.jpg" alt="Eu Sou Groot">
