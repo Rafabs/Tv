e1e66be14903a6a5c06b3465ac4d894c86b9e0f0
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-09-03 10:50:09 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,16 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
  
- if (data === '02/09') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E08-T3</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:50</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">(DES)ENCANTO - E02-T5</span></li> <hr class="filme-separador">    
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-    </ul>
-  `;
-} else if (data === '03/09') {
+ if (data === '03/09') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">17:10</span> <span class="label-leg">LEG</span> <span class="class-livre">L</span> <span class="filme">STAR WARS EPISÓDIO V: O IMPÉRIO CONTRA-ATACA</span></li> <hr class="filme-separador">
