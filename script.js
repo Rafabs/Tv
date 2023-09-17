feef2e234017f04518bc1a1e6fd0bf8fcee5df09
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-09-17 07:31:53 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '16/09') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E22-T3</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">DEPOIS DA CABANA - E06-T1</span></li> <hr class="filme-separador">           
-    <li><span class="horario">14:40</span> <span class="label-leg">LEG</span> <span class="class-0">0</span> <span class="filme">BLADE RUNNER</span></li> <hr class="filme-separador">           
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-    </ul>
-  `;       
-} else if (data === '17/09') {
+ if (data === '17/09') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">15:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E23-T3</span></li> <hr class="filme-separador">
