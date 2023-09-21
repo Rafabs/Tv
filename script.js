f73c33f2ef6f86f0ff63a1830d9406d43c784e9b
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-09-20 22:51:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,18 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '20/09') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E03-T4</span></li> <hr class="filme-separador">
-    <li><span class="horario">21:15</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">    
-    </ul>
-  `;  
-//  apagar primeiro item e confirmar qtd de episódio
-} else if (data === '21/09') {
+ if (data === '21/09') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
@@ -122,16 +111,6 @@ function mostrarProgramacao(button, data) {
 } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
-
-        <li class="filme-poster">
-          <img src="imgs//poster-sex-education.jpg" alt="Sex Education">
-          <div class="filme-texto">
-          <span class="dia">21/09</span>
-            <span class="class-16">16</span>
-            <span class="filme">SEX EDUCATION - T4</span>
-          </div>
-        </li>
-        <hr class="filme-separador">
 
         <li class="filme-poster">
           <img src="imgs//poster-destinos.jpg" alt="DESTINOS À DERIVA">
