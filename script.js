1bb8ee9d36ceb3b8afcd878057d9f75611b5e53e
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-10-01 15:03:26 -0300

    main

--- script.js
+++ script.js
@@ -10,16 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '30/09') {
-  programacaoDiv.innerHTML = `
-    <ul>
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E13-T4</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-livre">L</span> <span class="filme">E.T. O EXTRATERRESTRE</span></li> <hr class="filme-separador">
-    <li><span class="horario">18:00</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
-    </ul>
-  `;      
-} else if (data === '01/10') {
+  if (data === '01/10') {
   programacaoDiv.innerHTML = `
     <ul>
     <li><span class="horario">20:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E14-T4</span></li> <hr class="filme-separador">
