66a573aa5d0e371ae62a8c9d007b651a3018b0a8
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-08 07:23:18 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-  if (data === '07/10') {
-    programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E20-T4</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">LUPIN - E03-T3</span></li> <hr class="filme-separador">
-  <li><span class="horario">14:50</span> <span class="label-leg">LEG</span> <span class="class-livre">L</span> <span class="filme">A HISTÓRIA SEM FIM</span></li> <hr class="filme-separador">
-  <li><span class="horario">18:00</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
-  </ul>
-  `;
-  } else if (data === '08/10') {
+  if (data === '08/10') {
     programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">19:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E21-T4</span></li> <hr class="filme-separador">
