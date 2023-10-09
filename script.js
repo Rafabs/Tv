fbbe82dc60ee17d3e47d3bb1b80dffcc50d00e54
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-10-09 14:14:39 -0300

    main

--- script.js
+++ script.js
@@ -40,22 +40,22 @@ function mostrarProgramacao(button, data) {
   <li><span class="horario">19:35</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">LUPIN - E07-T3</span></li> <hr class="filme-separador">
   </ul>
   `;
-    // Excluir primeiro e segundo
   } else if (data === '12/10') {
     programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-  <li><span class="horario">08:30</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">ERA UMA VEZ UMA ESTRELA</span></li> <hr class="filme-separador">
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E01-T5</span></li> <hr class="filme-separador">
   <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A QUEDA DA CASA DE USHER - E01 - T1</span></li> <hr class="filme-separador">
   </ul>
   `;
+    // Excluir primeiro e segundo  
   } else if (data === '13/10') {
     programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+  <li><span class="horario">08:30</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">ERA UMA VEZ UMA ESTRELA</span></li> <hr class="filme-separador">
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E02-T5</span></li> <hr class="filme-separador">
