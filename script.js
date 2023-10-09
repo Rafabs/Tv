911608de2490ab9591cb21c1deb962e626bc0291
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-09 08:05:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,15 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-  if (data === '08/10') {
-    programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">19:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E21-T4</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:30</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">LUPIN - E04-T3</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
-  </ul>
-  `;
-  } else if (data === '09/10') {
+ if (data === '09/10') {
     programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
