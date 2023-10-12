ed5c5dc370717d06bf1df2f58210658f7e384229
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-10-12 10:59:21 -0300

    main

--- script.js
+++ script.js
@@ -72,6 +72,42 @@ function mostrarProgramacao(button, data) {
   <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A QUEDA DA CASA DE USHER - E06 - T1</span></li> <hr class="filme-separador">
   </ul>
   `;  
+} else if (data === '18/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+<li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E07-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">19:35</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A QUEDA DA CASA DE USHER - E07 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">21:30</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
+</ul>
+`;   
+// Excluir primeiro 
+} else if (data === '19/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E08-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A QUEDA DA CASA DE USHER - E08 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">20:50</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E01 - T1</span></li> <hr class="filme-separador">
+</ul>
+`; 
+// Excluir primeiro
+} else if (data === '20/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E09-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">LOKI - E03 - T2</span></li> <hr class="filme-separador">
+<li><span class="horario">20:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E02 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">21:40</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E01 - T7</span></li> <hr class="filme-separador">
+</ul>
+`;   
   } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
