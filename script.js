30ec7005d4e0ccfcf1e2dcb76481ee0730cc8a25
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-13 08:51:28 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,18 +10,6 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '12/10') {
-    programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E01-T5</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:35</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A QUEDA DA CASA DE USHER - E01 - T1</span></li> <hr class="filme-separador">
-  <li><span class="horario">21:20</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">ELIMINATÓRIAS DA COPA</span></li> <hr class="filme-separador">
-  </ul>
-  `;
-    // Excluir primeiro
   } else if (data === '13/10') {
     programacaoDiv.innerHTML = `
   <ul>
