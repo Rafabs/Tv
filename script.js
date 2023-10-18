eeb181c26123e15fa15bbfbc74552fcf669b8a63
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-10-18 11:09:52 -0300

    main

--- script.js
+++ script.js
@@ -44,6 +44,38 @@ function mostrarProgramacao(button, data) {
 <li><span class="horario">20:00</span> <span class="label-leg">DUB</span> <span class="class-14">14</span> <span class="filme">LOKI - E03 - T2</span></li> <hr class="filme-separador">
 <li><span class="horario">20:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E02 - T1</span></li> <hr class="filme-separador">
 <li><span class="horario">21:40</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E01 - T7</span></li> <hr class="filme-separador">
+<li><span class="horario">23:00</span> <span class="label-leg">DUB</span> <span class="class-16">16</span> <span class="filme">TIOZÕES</span></li> <hr class="filme-separador">
+</ul>
+`;   
+} else if (data === '21/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E10-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E03 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">14:50</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E02 - T7</span></li> <hr class="filme-separador">
+<li><span class="horario">15:50</span> <span class="label-leg">LEG</span> <span class="class-0">0</span> <span class="filme">MAD MAX 3</span></li> <hr class="filme-separador">
+<li><span class="horario">18:00</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
+</ul>
+`;  
+} else if (data === '22/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E11-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E04 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">14:50</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E03 - T7</span></li> <hr class="filme-separador">
+<li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
+</ul>
+`;    
+} else if (data === '23/10') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - 12-T5</span></li> <hr class="filme-separador">
+<li><span class="horario">20:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E05 - T1</span></li> <hr class="filme-separador">
+<li><span class="horario">21:40</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E04 - T7</span></li> <hr class="filme-separador">
 </ul>
 `;   
   } else if (data === 'em-breve') {
