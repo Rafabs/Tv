82e14a409fa265c29e3a2118441110239a7042c7
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-21 08:48:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,20 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
   
- if (data === '20/10') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E09-T5</span></li> <hr class="filme-separador">
-<li><span class="horario">20:00</span> <span class="label-leg">DUB</span> <span class="class-14">14</span> <span class="filme">LOKI - E03 - T2</span></li> <hr class="filme-separador">
-<li><span class="horario">20:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E02 - T1</span></li> <hr class="filme-separador">
-<li><span class="horario">21:40</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ELITE - E01 - T7</span></li> <hr class="filme-separador">
-<li><span class="horario">23:00</span> <span class="label-leg">DUB</span> <span class="class-16">16</span> <span class="filme">TIOZÕES</span></li> <hr class="filme-separador">
-</ul>
-`;   
-} else if (data === '21/10') {
+ if (data === '21/10') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
