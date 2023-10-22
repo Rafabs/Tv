18f4b4a3c3b0a6d7a2ba413d10b8348067f8b97e
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-22 20:22:23 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,16 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
   
- if (data === '21/10') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-<li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E10-T5</span></li> <hr class="filme-separador">
-<li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CORPOS - E03 - T1</span></li> <hr class="filme-separador">
-<li><span class="horario">18:00</span> <span class="label-orig">NAC</span> <span class="class-livre">L</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
-</ul>
-`;  
-} else if (data === '22/10') {
+ if (data === '22/10') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E11-T5</span></li> <hr class="filme-separador">
