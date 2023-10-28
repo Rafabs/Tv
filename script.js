8809b2f8742c7fc6decb55fd93230137fff7d5f0
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-10-28 08:21:56 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,18 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
   
- if (data === '27/10') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E16-T5</span></li> <hr class="filme-separador">
-<li><span class="horario">20:00</span> <span class="label-leg">DUB</span> <span class="class-14">14</span> <span class="filme">LOKI - E04 - T2</span></li> <hr class="filme-separador">
-<li><span class="horario">22:35</span> <span class="label-leg">DUB</span> <span class="class-0">0</span> <span class="filme">LEGO MARVEL VINGADORES: CÓDIGO VERMELHO</span></li> <hr class="filme-separador">
-</ul>
-`; 
-} else if (data === '28/10') {
+ if (data === '28/10') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
