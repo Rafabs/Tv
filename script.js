041890e52b53947bab1d081bce9eb1d3c4b8f2c3
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2023-11-08 13:38:29 -0300

    MAIN

--- script.js
+++ script.js
@@ -42,6 +42,30 @@ function mostrarProgramacao(button, data) {
 <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">FERRY: A SÉRIE - E08-T1</span></li> <hr class="filme-separador">
 </ul>
 `;
+} else if (data === '11/11') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">13:30</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - 07-T6</span></li> <hr class="filme-separador">
+<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+</ul>
+`;
+} else if (data === '12/11') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">20:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - 08-T6</span></li> <hr class="filme-separador">
+<li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
+</ul>
+`;
+} else if (data === '13/11') {
+  programacaoDiv.innerHTML = `
+<ul>
+<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
+<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - 09-T6</span></li> <hr class="filme-separador">
+</ul>
+`;
   } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
