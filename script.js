884931af7ea5066f2a853369db9c17879d15b890
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-11-16 07:48:35 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
   
- if (data === '15/11') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
-<li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-<li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-<li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E11-T6</span></li> <hr class="filme-separador">
-</ul>
-`;
-// excluir primeiro e confirmar qtd
-} else if (data === '16/11') {
+ if (data === '16/11') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">06:00</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">BOM DIA SP</span></li> <hr class="filme-separador">
@@ -72,16 +62,6 @@ function mostrarProgramacao(button, data) {
   } else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
-        
-        <li class="filme-poster">
-          <img src="imgs//poster-crown.jpg" alt="THE CROWN - T6 - PARTE 1">
-          <div class="filme-texto">
-          <span class="dia">16/11</span>
-            <span class="class-16">16</span>
-            <span class="filme">THE CROWN - T6 - PARTE 1</span>
-          </div>
-        </li>
-        <hr class="filme-separador">
 
         <li class="filme-poster">
           <img src="imgs//poster-sweet.jpg" alt=SWEET HOME - T2">
