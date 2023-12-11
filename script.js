49e674817d28a9f27b3cbb20dd8abaa791d0b417
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-12-11 07:54:17 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,15 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '10/12') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">17:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">O MUNDO DEPOIS DE NÓS</span></li> <hr class="filme-separador">
-<li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E12-T7</span></li> <hr class="filme-separador">
-<li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
-</ul>
-`;
-} else if (data === '11/12') {
+ if (data === '11/12') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
