7c8bc245f182e6ed4c38a467f480501cb9486d9a
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2023-12-18 08:28:18 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,16 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '17/12') {
-  programacaoDiv.innerHTML = `
-<ul>
-<li><span class="horario">17:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E19-T7</span></li> <hr class="filme-separador">
-<li><span class="horario">17:30</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">THE CROWN - 09-T6</span></li> <hr class="filme-separador">
-<li><span class="horario">18:30</span> <span class="label-dub">DUB</span> <span class="class-livre">L</span> <span class="filme">A FUGA É DAS GALINHAS</span></li> <hr class="filme-separador">
-<li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
-</ul>
-`;
-} else if (data === '18/12') {
+ if (data === '18/12') {
   programacaoDiv.innerHTML = `
 <ul>
 <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
