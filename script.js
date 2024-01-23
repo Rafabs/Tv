e6d21d5ed8dc150ce9850944bce830a1a90ba7e3
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-01-22 23:25:04 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '22/01') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E21 - T8</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">KUBRA - E03 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:45</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">A HERDEIRA - E01 - T01</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '23/01') {
+ if (data === '23/01') {
   programacaoDiv.innerHTML = `
   <ul>
   <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-dub">DUB</span> <span class="class-livre">L</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">DETETIVE PIKACHU</span> </div> </li> <hr class="filme-separador"> </li>
