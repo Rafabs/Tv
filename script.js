6ed1c91d9d78928b8159427aba49d6efe06f5328
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-01-28 22:30:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,16 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '28/01') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">19:20</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E02 - T9</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">A HERDEIRA - E06 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '29/01') {
+ if (data === '29/01') {
   programacaoDiv.innerHTML = `
   <ul>
   <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">ANIMAIS FANTÁSTICOS: O CRIME DE GRINDEWALD</span> </div> </li> <hr class="filme-separador"> </li>
