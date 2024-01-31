87ac9cf9f3b246a9bfd9e82327f3f5d5d65e476c
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-01-31 17:37:36 -0300

    main

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ else if (data === '01/02') {
 else if (data === '02/02') {
   programacaoDiv.innerHTML = `
   <ul>
+  <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">PERCY JACKSON E OS OLÍMPIANOS - E08 - T01</span> </div> </li> <hr class="filme-separador"> </li>
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
   <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E06 - T9</span></li> <hr class="filme-separador">
   </ul>
