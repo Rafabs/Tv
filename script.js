7196235229cee8c1f3f433a1bf4be1112eecd6dc
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-02-06 13:49:12 -0300

    main

--- script.js
+++ script.js
@@ -67,13 +67,24 @@ else if (data === '14/02') {
   </ul>
 `;
 }
+// excluir primeiro
 else if (data === '15/02') {
   programacaoDiv.innerHTML = `
   <ul>
   <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">ECO - E03 - T01</span> </div> </li> <hr class="filme-separador"> </li>
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E13 - T9</span></li> <hr class="filme-separador">
+  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E14 - T9</span></li> <hr class="filme-separador">
+  <li><span class="horario">20:00</span> <span class="label-orig">NAC</span> <span class="class-18">18</span> <span class="filme">BOM DIA, VERONICA: A CAÇADA FINAL - E01 - T04</span></li> <hr class="filme-separador">
+  </ul>
+`;
+}
+else if (data === '16/02') {
+  programacaoDiv.innerHTML = `
+  <ul>
+  <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">ECO - E04 - T01</span> </div> </li> <hr class="filme-separador"> </li>
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E14 - T9</span></li> <hr class="filme-separador">
   </ul>
 `;
 }
