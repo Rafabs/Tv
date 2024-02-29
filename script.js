88b5ebe204c2cbb08805e492f7e13db6c2ad058d
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-02-29 10:23:17 -0300

    main

--- script.js
+++ script.js
@@ -11,18 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '28/02') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E23 - T9</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-10">10</span> <span class="filme">AVATAR: O ÚLTIMO MESTRE DO AR - E04 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">21:35</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CAMPEONATO PAULISTA</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '29/02') {
+if (data === '29/02') {
   programacaoDiv.innerHTML = `
   <ul>
   <li class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">SHAZAM! FÚRIA DOS DEUSES</span> </div> </li> <hr class="filme-separador"> </li>
