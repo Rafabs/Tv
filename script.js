4e67747bd0622400baa245ea255da456d339c706
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-03-05 21:50:44 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,18 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '05/03') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li> <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">AS MARVELS</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E03 - T10</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-10">10</span> <span class="filme">AVATAR: O ÚLTIMO MESTRE DO AR - E08 - T01</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '06/03') {
+ if (data === '06/03') {
   programacaoDiv.innerHTML = `
   <ul>
   <li> <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O ASTRONAUTA</span> </div> </li> <hr class="filme-separador"> </li>
