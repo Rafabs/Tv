1dece3dca90e09903e671300abd7e91e18fef1eb
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-03-07 18:57:39 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,17 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '06/03') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li> <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O ASTRONAUTA</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E03 - T10</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:45</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">F1: DIRIGIR PARA VIVER - E01 - T06</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '08/03') {
+ if (data === '08/03') {
   programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
