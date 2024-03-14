2bcda287c92049f0ecb022d7466fcd851c798ac6
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-03-13 23:40:42 -0300

    main

--- script.js
+++ script.js
@@ -11,17 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '13/03') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E07 - T10</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">F1: DIRIGIR PARA VIVER - E06 - T06</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '14/03') {
+ if (data === '14/03') {
   programacaoDiv.innerHTML = `
   <ul>
   <li> <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">MORTAL KOMBAT</span> </div> </li> <hr class="filme-separador"> </li>
@@ -176,6 +166,16 @@ else if (data === '24/03') {
     <span class="class-16">16</span>
     <span class="filme">BEAUTIFUL REBEL</span>
   </div>
+</li>
+<hr class="filme-separador">
+
+<li class="filme-poster">
+<img src="imgs//poster-atlas.jpg" alt="ATLAS">
+<div class="filme-texto">
+<span class="dia">24/05</span>
+  <span class="class-12">12</span>
+  <span class="filme">ATLAS</span>
+</div>
 </li>
 <hr class="filme-separador">
 
