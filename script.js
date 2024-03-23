d07e234a07fef6e50b80185cca271e51b92635ab
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-03-22 23:53:22 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,17 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 // 07/02 - as marvels
 
- if (data === '20/03') {
-  programacaoDiv.innerHTML = `
-  <ul>
-  <li> <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">AQUAMAN 2: O REINO PERDIDO</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E12 - T10</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:45</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">X-MEN '97 - E01 - T01</span></li> <hr class="filme-separador">
-  </ul>
-`;
-}
-else if (data === '22/03') {
+ if (data === '22/03') {
   programacaoDiv.innerHTML = `
   <ul>
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
