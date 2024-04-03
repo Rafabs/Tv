c1e92661a5007a06655ee84cd4ff86c2516ae7d4
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-04-02 22:48:50 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '02/04') {
-    programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">00:00</span> <span class="label-dub">DUB</span> <span class="class-livre">L</span> <span class="filme">SUPER MÁRIO BROS: O FILME</span></li> <hr class="filme-separador">
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E23 - T10</span></li> <hr class="filme-separador">
-  </ul>
-`;
-  }   
-  else if (data === '03/04') {
+ if (data === '03/04') {
     programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">TREM BALA</span></li> <hr class="filme-separador">
