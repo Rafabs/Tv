c4200601e66f67ba1b63946e86795a7dc1349283
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-04-11 01:22:36 -0300

    main

--- script.js
+++ script.js
@@ -30,11 +30,35 @@ function mostrarProgramacao(button, data) {
     <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">RIPLEY - E07 - T01</span></li> <hr class="filme-separador">
     </ul>
 `;
-  }       
+  }     
+  else if (data === '15/04') {
+    programacaoDiv.innerHTML = `
+    <ul>  
+    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+    <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E08 - T11</span></li> <hr class="filme-separador">
+    <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">RIPLEY - E08 - T01</span></li> <hr class="filme-separador">
+    <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">PARASYTE: THE GREY - E04 - T01</span></li> <hr class="filme-separador">
+    </ul>
+`;
+  }    
+  else if (data === '16/04') {
+    programacaoDiv.innerHTML = `
+    <ul>  
+    <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">HERANÇA ROUBADA</span> </div> </li> <hr class="filme-separador"> </li>
+    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
+    <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E08 - T11</span></li> <hr class="filme-separador">
+    <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">RIPLEY - E08 - T01</span></li> <hr class="filme-separador">
+    <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">PARASYTE: THE GREY - E04 - T01</span></li> <hr class="filme-separador">
+    </ul>
+`;
+  }        
   else if (data === '17/04') {
     programacaoDiv.innerHTML = `
   <ul>  
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
+  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E09 - T11</span></li> <hr class="filme-separador">
   <li><span class="horario">13:45</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">X-MEN '97 - E06 - T01</span></li> <hr class="filme-separador">
+  <li><span class="horario">14:15</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">PARASYTE: THE GREY - E05 - T01</span></li> <hr class="filme-separador">
   </ul>
 `;
   }    
