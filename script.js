6a5061107fefdb73874695efe4ba74015f0f497a
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-04-10 23:23:01 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,19 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 
 
- if (data === '10/04') {
-    programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">18:10</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">PARASYTE: THE GREY - E02 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E05 - T11</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">X-MEN '97 - E05 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">20:30</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">RIPLEY - E05 - T01</span></li> <hr class="filme-separador">
-  <li><span class="horario">21:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CONMEBOL LIBERTADORES</span></li> <hr class="filme-separador">
-  </ul>
-`;
-  }
-  else if (data === '11/04') {
+ if (data === '11/04') {
     programacaoDiv.innerHTML = `
   <ul>  
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O QUE JENNIFER FEZ?</span> </div> </li> <hr class="filme-separador"> </li>
