cc4b865434cbda8b29cf457e93511cf48e0e4421
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-04-27 19:21:25 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -11,16 +11,7 @@ function mostrarProgramacao(button, data) {
   button.classList.add("active");
 
 
-if (data === '26/04') {
-    programacaoDiv.innerHTML = `
-  <ul>  
-  <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-10">10</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">PLANETA VIVO - E04 - T01</span> </div> </li> <hr class="filme-separador"> </li>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E16 - T11</span></li> <hr class="filme-separador">
-  </ul>
-`;
-  }    
-  else if (data === '29/04') {
+ if (data === '29/04') {
     programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">
