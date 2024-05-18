98003bd399b2e656b9e5f80531d764f472a89f2f
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-05-18 00:52:10 -0300

    main

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ if (data === '18/05') {
   }   
   else if (data === '20/05') {
     programacaoDiv.innerHTML = `
-    <ul>  
+    <ul>
+    <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">DOCTOR WHO - E04 - T01</span> </div> </li> <hr class="filme-separador"> </li>    
     <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
     <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">THE BIG BANG THEORY - E10 - T12</span></li> <hr class="filme-separador">
     </ul>
