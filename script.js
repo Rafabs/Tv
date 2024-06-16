348679d202d63e6c415910baeb8f35a8a38e7e76
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-06-16 10:14:54 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,20 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '15/06') {
-    programacaoDiv.innerHTML = `
-    <ul>  
-    <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">A CASA DO DRAGÃO - E08 - T01</span> </div> </li> <hr class="filme-separador"> </li>
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E05 - T01</span></li> <hr class="filme-separador">
-    <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-10">10</span> <span class="filme">SWEET TOOTH - E06 - T03</span></li> <hr class="filme-separador">
-    <li><span class="horario">14:40</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">TOUR DE FRANCE: NO CORAÇÃO DO PELOTÃO - E05 - T02</span></li> <hr class="filme-separador">
-    <li><span class="horario">15:40</span> <span class="label-leg">LEG</span> <span class="class-livre">L</span> <span class="filme">HARRY POTTER E A PEDRA FILOSOFAL</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-    </ul>
-`;
-  }      
-  else if (data === '16/06') {
+ if (data === '16/06') {
     programacaoDiv.innerHTML = `
     <ul>  
     <li><span class="horario">15:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">
