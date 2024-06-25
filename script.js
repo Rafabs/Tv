30033c7ac828a621e7c5dd8bd0b7e36bbe7d596b
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-06-25 00:02:45 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '24/06') {
-    programacaoDiv.innerHTML = `
-    <ul>  
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-    <li><span class="horario">18:45</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E09 - T01</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:10</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">SWEET TOOTH - E08 - T03</span></li> <hr class="filme-separador">
-    <li><span class="horario">19:30</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">TOUR DE FRANCE: NO CORAÇÃO DO PELOTÃO - E06 - T02</span></li> <hr class="filme-separador">
-    </ul>
-    `;
-  }    
-  else if (data === '25/06') {
+ if (data === '25/06') {
     programacaoDiv.innerHTML = `
     <ul>  
     <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">THE ACOLYTE - E04 - T01</span> </div> </li> <hr class="filme-separador"> </li>    
