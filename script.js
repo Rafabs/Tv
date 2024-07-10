4cc37c330e0379b35c0a1cde17077ebaebf88818
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-07-09 23:37:56 -0300

    MAIN

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
- if (data === '09/07') {
-    programacaoDiv.innerHTML = `
-    <ul>  
-    <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">TRANSFORMERS: O DESPERTAR DAS FERAS</span> </div> </li> <hr class="filme-separador"> </li>    
-    <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-    <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E21 - T01</span></li> <hr class="filme-separador">    
-    <li><span class="horario">19:35</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">THE ACOLYTE - E07 - T01</span></li> <hr class="filme-separador">    
-    </ul>
-`;
-  }   
-  else if (data === '10/07') {
+ if (data === '10/07') {
     programacaoDiv.innerHTML = `
     <ul>  
     <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">GODZILLA E KONG: UM NOVO IMPÉRIO</span> </div> </li> <hr class="filme-separador"> </li>    
@@ -43,7 +33,6 @@ function mostrarProgramacao(button, data) {
     <ul>  
     <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
     <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E02 - T02</span></li> <hr class="filme-separador">    
-    <li><span class="horario">13:25</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">CIDADE DE DEUS</span></li> <hr class="filme-separador">    
     <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
     </ul>
 `;
@@ -51,6 +40,7 @@ function mostrarProgramacao(button, data) {
   else if (data === '14/07') {
     programacaoDiv.innerHTML = `
     <ul>  
+    <li><span class="horario">18:00</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">CIDADE DE DEUS</span></li> <hr class="filme-separador">    
     <li><span class="horario">20:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E03 - T02</span></li> <hr class="filme-separador">    
     <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador">    
     <li><span class="horario">22:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">A CASA DO DRAGÃO - E05 - T02</span></li> <hr class="filme-separador">
