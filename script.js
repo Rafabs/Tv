92237e06fa6c0a6d145d9ef282f5d65cd235a83a
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-08-15 23:41:25 -0300

    main

--- script.js
+++ script.js
@@ -9,18 +9,8 @@ function mostrarProgramacao(button, data) {
 
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
-
- if (data === '15/08') {
-    programacaoDiv.innerHTML = `
-    <ul>  
-    <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-    <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-    <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E04 - T03</span></li> <hr class="filme-separador">    
-    <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">THE UMBRELLA ACADEMY - E05 - T04</span></li> <hr class="filme-separador">    
-    </ul>
-`;
-  }  
-  else if (data === '16/08') {
+ 
+  if (data === '16/08') {
     programacaoDiv.innerHTML = `
     <ul>  
     <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
