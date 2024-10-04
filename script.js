fe640ccaa9d8e944cad4936e0efe62ba20d6417c
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-10-04 18:06:10 -0300

    MAIN

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ else if (data === '09/10') {
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E04 - T05</span></li> <hr class="filme-separador">    
-  <li><span class="horario">23:45</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">AGATHA DESDE SEMPRE - E05 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">AGATHA DESDE SEMPRE - E05 - T01</span></li> <hr class="filme-separador">    
   </ul>
 `;
 }
