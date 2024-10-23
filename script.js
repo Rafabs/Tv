5c91a9c4efe9693bc77e3b8fd43132ba759ae151
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-10-23 15:56:50 -0300

    MAIN

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function mostrarProgramacao(button, data) {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E14 - T05</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">TOMB RAIDER: A LENDA DE LARA CROFT - E07 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E14 - T05</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:35</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">TOMB RAIDER: A LENDA DE LARA CROFT - E07 - T01</span></li> <hr class="filme-separador">    
   <li><span class="horario">23:45</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">AGATHA DESDE SEMPRE - E07 - T01</span></li> <hr class="filme-separador">    
   </ul>
 `;
