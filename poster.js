8834feb2bc9bbeffe8fe8018edc1993a64e3f757
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-10-28 22:53:46 -0300

    Atualizar o poster.js

--- poster.js
+++ poster.js
@@ -6,15 +6,6 @@ function exibirProgramacao() {
     
     var diasProgramacao = [
         {
-            data: "2024-10-28",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "YOUNG SHELDON - E19 - T05", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:00", titulo: "PINGUIM - E06 - T01", poster: "poster-pinguim.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` }
-            ]
-        },
-        {
             data: "2024-10-29",
             sessoes: [
                 { horario: "00:01", titulo: "A EXTRAORDINÁRIA VIDA DE IBELIN", poster: "poster-ibelin.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
