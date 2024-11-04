908959ec45aaa4ecac12bf99e9bd9dada6302bc4
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-11-04 14:45:56 -0300

    main

--- poster.js
+++ poster.js
@@ -6,49 +6,66 @@ function exibirProgramacao() {
     
     var diasProgramacao = [    
         {
-            data: "2024-11-01",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "YOUNG SHELDON - E22 - T05", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:00", titulo: "AGATHA DESDE SEMPRE - E08 - T01", poster: "poster-agatha.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
-                { horario: "20:40", titulo: "A ÚLTIMA NOITE EM TREMOR - E02 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` }
-            ]
-        },   
-        {
-            data: "2024-11-02",
+            data: "2024-11-05",
             sessoes: [
                 { horario: "00:01", titulo: "O QUE TIVER QUE SER", poster: "poster-tiver.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
                 { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "13:25", titulo: "YOUNG SHELDON - E01 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "13:45", titulo: "AGATHA DESDE SEMPRE - E09 - T01", poster: "poster-agatha.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
-                { horario: "14:25", titulo: "A ÚLTIMA NOITE EM TREMOR - E03 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-            ]
-        },         
-        {
-            data: "2024-11-03",
-            sessoes: [
-                { horario: "14:25", titulo: "ABDUÇÃO EM MANHATTAN - E01 - T01", poster: "poster-abducao.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
-                { horario: "19:00", titulo: "A ÚLTIMA NOITE EM TREMOR - E04 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
-                { horario: "20:10", titulo: "YOUNG SHELDON - E02 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:30", titulo: "FANTÁSTICO", poster: "poster-fantastico.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "22:00", titulo: "PINGUIM - E07 - T01", poster: "poster-pinguim.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
-            ]
-        },          
-        {
-            data: "2024-11-04",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
                 { horario: "13:25", titulo: "YOUNG SHELDON - E03 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
             ]
-        },       
+        },  
+        {
+            data: "2024-11-06",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "18:45", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:15", titulo: "YOUNG SHELDON - E04 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "19:35", titulo: "A ÚLTIMA NOITE EM TREMOR - E05 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
+                { horario: "20:40", titulo: "ABDUÇÃO EM MANHATTAN - E03 - T01", poster: "poster-abducao.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
+            ]
+        },   
+        {
+            data: "2024-11-07",
+            sessoes: [
+                { horario: "00:01", titulo: "PEDRO PÁRAMO", poster: "poster-paramo.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-16">16</span>` },
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON - E05 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "20:00", titulo: "A ÚLTIMA NOITE EM TREMOR - E06 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
+            ]
+        },     
+        {
+            data: "2024-11-08",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "13:25", titulo: "YOUNG SHELDON - E06 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+            ]
+        },                                 
         {
             data: "2024-11-11",
             sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON - E07 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
                 { horario: "20:00", titulo: "PINGUIM - E08 - T01", poster: "poster-pinguim.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "20:50", titulo: "A ÚLTIMA NOITE EM TREMOR - E07 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
             ]
         },        
+        {
+            data: "2024-11-12",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON - E08 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "20:00", titulo: "A ÚLTIMA NOITE EM TREMOR - E08 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
+            ]
+        },    
+        {
+            data: "2024-11-13",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "13:25", titulo: "YOUNG SHELDON - E08 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+            ]
+        },                    
     ];
 
     var dataAtual = now.toISOString().split('T')[0];
--- script.js
+++ script.js
@@ -10,24 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '02/11') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">AGATHA DESDE SEMPRE - E09 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">14:25</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A ÚLTIMA NOITE EM TREMOR - E03 - T01</span></li> <hr class="filme-separador">    
-  </ul>
-`;
-}
-else if (data === '03/11') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">18:20</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">ABDUÇÃO EM MANHATTAN - E01 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A ÚLTIMA NOITE EM TREMOR - E04 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">22:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">PINGUIM - E07 - T01</span></li> <hr class="filme-separador">    
-  </ul>
-`;
-}
-else if (data === '05/11') {
+if (data === '05/11') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O QUE TIVER QUE SER</span> </div> </li> <hr class="filme-separador"> </li>
@@ -51,8 +34,8 @@ else if (data === '07/11') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">PEDRO PÁRAMO</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">      <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">      <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E05 - T06</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">A ÚLTIMA NOITE EM TREMOR - E06 - T01</span></li> <hr class="filme-separador">    
   </ul>
