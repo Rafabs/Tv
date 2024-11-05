b061c52bf753841653734a31d99f0dcdb9167101
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-11-05 20:07:32 -0300

    Atualizar o poster.js

--- poster.js
+++ poster.js
@@ -4,15 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [    
-        {
-            data: "2024-11-05",
-            sessoes: [
-                { horario: "00:01", titulo: "O QUE TIVER QUE SER", poster: "poster-tiver.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "13:25", titulo: "YOUNG SHELDON - E03 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-            ]
-        },  
+    var diasProgramacao = [      
         {
             data: "2024-11-06",
             sessoes: [
