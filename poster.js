bd39353366ef8a713043e94bbca9e25fec86d711
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-11-07 23:50:36 -0300

    main

--- poster.js
+++ poster.js
@@ -4,17 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [       
-        {
-            data: "2024-11-07",
-            sessoes: [
-                { horario: "00:01", titulo: "PEDRO PÁRAMO", poster: "poster-paramo.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-16">16</span>` },
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "YOUNG SHELDON - E05 - T06", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:00", titulo: "A ÚLTIMA NOITE EM TREMOR - E06 - T01", poster: "poster-tremor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
-            ]
-        },     
+    var diasProgramacao = [            
         {
             data: "2024-11-08",
             sessoes: [
