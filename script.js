8f8cde6d1552128383b6425a2670a4459bb9d8a2
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-11-25 23:27:02 -0300

    main

--- script.js
+++ script.js
@@ -48,6 +48,16 @@ else if (data === '29/11') {
   </ul>
 `;
 }
+else if (data === '30/11') {
+  programacaoDiv.innerHTML = `
+  <ul>  
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
+  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E20 - T06</span></li> <hr class="filme-separador">    
+  <li><span class="horario">13:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SENNA - E02 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
+  </ul>
+`;
+}
   // <li <class="filme-container"> <div class="filme-info"> <span class="horario">01:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">TESTE TESTE TESTE TESTE</span> </div> </li> <hr class="filme-separador"> </li>
 
   else if (data === 'em-breve') {
