e624b06f0f9d4b6181d700abfe99c6638cbab050
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-11-27 00:15:05 -0300

    Atualizar o poster.js

--- poster.js
+++ poster.js
@@ -4,15 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                              
-        {
-            data: "2024-11-26",
-            sessoes: [
-                { horario: "23:59", titulo: "PIANO DE FAMÍLIA", poster: "poster-piano.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-            ]
-        },   
+    var diasProgramacao = [                                                                                                 
         {
             data: "2024-11-27",
             sessoes: [
