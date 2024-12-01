2dea444aa0d08771a287d486f1e66eed088b7c2e
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-12-01 00:19:33 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
  
-if (data === '30/11') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E20 - T06</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:45</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">SENNA - E02 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-  </ul>
-`;
-}
-else if (data === '01/12') {
+ if (data === '01/12') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E21 - T06</span></li> <hr class="filme-separador">    
