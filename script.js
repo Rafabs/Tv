be1b8751da7229176f93e22ee6ba705a279ca641
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-12-04 19:06:40 -0300

    main

--- script.js
+++ script.js
@@ -22,24 +22,31 @@ function mostrarProgramacao(button, data) {
   </ul>
 `;
 }
+else if (data === '05/12') {
+  programacaoDiv.innerHTML = `
+  <ul>  
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E02 - T07</span></li> <hr class="filme-separador">    
+  <li><span class="horario">20:00</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">SENNA - E05 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">21:15</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">STAR WARS: SKELETON CREW - E02 - T01</span></li> <hr class="filme-separador">    
+  </ul>
+`;
+}
+else if (data === '06/12') {
+  programacaoDiv.innerHTML = `
+  <ul>  
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">CHURCHILL EM GUERRA - E01 - T01</span> </div> </li> <hr class="filme-separador"> </li>
+  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
+  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E03 - T07</span></li> <hr class="filme-separador">    
+  </ul>
+`;
+}
   // <li <class="filme-container"> <div class="filme-info"> <span class="horario">01:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">TESTE TESTE TESTE TESTE</span> </div> </li> <hr class="filme-separador"> </li>
 
   else if (data === 'em-breve') {
     programacaoDiv.innerHTML = `    
       <ul>
-
-<li class="filme-poster">
-<img src="imgs//poster-star.jpg" alt="STAR WARS: SKELETON CREW">
-<div class="filme-texto">
-<span class="dia">03/12</span>
-<span class="class-12">12</span>
-<span class="filme">STAR WARS: SKELETON CREW</span>
-<div class="filme-texto">
-<span class="class-serie">Série</span>
-<span class="class-temporada">T1</span>
-</div>
-</li>
-<hr class="filme-separador">
 
 <li class="filme-poster">
 <img src="imgs//poster-churchill.jpg" alt="churchill">
