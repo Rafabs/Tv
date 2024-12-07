0b4f6d2d549708ddfe75d9e1cc3eb551f7b5334d
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2024-12-07 07:34:57 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -9,17 +9,8 @@ function mostrarProgramacao(button, data) {
 
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
-
-if (data === '06/12') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">CHURCHILL EM GUERRA - E01 - T01</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E03 - T07</span></li> <hr class="filme-separador">    
-  </ul>
-`;
-}
-else if (data === '09/12') {
+  
+   if (data === '09/12') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
