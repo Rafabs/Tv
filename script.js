d585053ae9504e823aa99194a846083feaca371a
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-12-10 15:25:28 -0300

    main

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ if (data === '10/12') {
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E05 - T07</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">CHURCHILL EM GUERRA - E03 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CHURCHILL EM GUERRA - E03 - T01</span></li> <hr class="filme-separador">    
   <li><span class="horario">23:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">STAR WARS: SKELETON CREW - E03 - T01</span></li> <hr class="filme-separador">    
   </ul>
 `;
@@ -26,7 +26,7 @@ else if (data === '11/12') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">A BALEIA</span> </div> </li> <hr class="filme-separador"> </li>
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
+  <li><span class="horario">11:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
   <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E06 - T07</span></li> <hr class="filme-separador">    
   </ul>
 `;
@@ -37,8 +37,8 @@ else if (data === '14/12') {
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:01</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">OS FANTASMAS AINDA SE DIVERTEM</span> </div> </li> <hr class="filme-separador"> </li>
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
   <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">YOUNG SHELDON - E07 - T07</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:45</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">CHURCHILL EM GUERRA - E04 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">14:45</span> <span class="label-orig">NAC</span> <span class="class-16">16</span> <span class="filme">QUEM VÊ CASA... - E01 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">CHURCHILL EM GUERRA - E04 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">14:45</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">QUEM VÊ CASA... - E01 - T01</span></li> <hr class="filme-separador">    
   </ul>
 `;
 }
