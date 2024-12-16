4b034c7a3b867e6af420d77d727929a48d90ad74
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2024-12-15 23:34:03 -0300

    MAIN

--- poster.js
+++ poster.js
@@ -4,16 +4,68 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                                                              
+    var diasProgramacao = [                                                                                                                                        
         {
-            data: "2024-12-14",
+            data: "2024-12-16",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "13:25", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+            ]
+        },   
+        {
+            data: "2024-12-18",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "20:00", titulo: "CHURCHILL EM GUERRA", poster: "poster-churchill.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "20:45", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+            ]
+        },   
+        {
+            data: "2024-12-19",
+            sessoes: [
+                { horario: "00:01", titulo: "CORINGA: DELÍRIO À DOIS", poster: "poster-coringa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "20:00", titulo: "CHURCHILL EM GUERRA", poster: "poster-churchill.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "20:45", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+            ]
+        },   
+        {
+            data: "2024-12-20",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "13:25", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+            ]
+        },   
+        {
+            data: "2024-12-23",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "19:40", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
+                { horario: "20:00", titulo: "CHURCHILL EM GUERRA", poster: "poster-churchill.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "20:45", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+            ]
+        },   
+        {
+            data: "2024-12-24",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
                 { horario: "13:25", titulo: "YOUNG SHELDON", poster: "poster-sheldon.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
                 { horario: "13:45", titulo: "CHURCHILL EM GUERRA", poster: "poster-churchill.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
-                { horario: "14:45", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "14:25", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
             ]
-        },                                                                                                      
+        },   
+        {
+            data: "2024-12-26",
+            sessoes: [
+                { horario: "20:00", titulo: "CHURCHILL EM GUERRA", poster: "poster-churchill.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+                { horario: "20:45", titulo: "QUEM VÊ CASA...", poster: "poster-casa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
+            ]
+        },                                                                                                                                                     
     ];
 
     var dataAtual = now.toISOString().split('T')[0];
