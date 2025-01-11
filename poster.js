1b87bce1a725ff129de5d4ef62e3e8b560199be5
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2025-01-10 23:41:44 -0300

    main

--- poster.js
+++ poster.js
@@ -4,16 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                                                                                                                                                                                                                                               
-        {
-            data: "2025-01-10",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "AEROPORTO - ÁREA RESTRITA", poster: "poster-aeroporto.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-14">14</span>` },
-                { horario: "20:05", titulo: "TERRA INDOMÁVEL", poster: "poster-indomavel.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },                
-            ]
-        },       
+    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                 
         {
             data: "2025-01-11",
             sessoes: [
