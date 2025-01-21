d3bd0bc7c12c8307c9bc484cbc9915a5e9ca1bfe
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2025-01-21 03:27:32 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '20/01') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">18:35</span> <span class="label-orig">NAC</span> <span class="class-14">14</span> <span class="filme">AEROPORTO - ÁREA RESTRITA - E04 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">18:05</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">TERRA INDOMÁVEL - E05 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:40</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CAMPEONATO PAULISTA</span></li> <hr class="filme-separador">    
-`;
-}
-else if (data === '21/01') {
+ if (data === '21/01') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
