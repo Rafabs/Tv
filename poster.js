7225c77d97a04461e2d0b02c6897975d550db9c4
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2025-03-13 23:29:01 -0300

    main
    Atualização 13/03, adição e remoção de itens.

--- poster.js
+++ poster.js
@@ -7,16 +7,6 @@ function exibirProgramacao() {
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
                       
         {
-            data: "2025-03-13",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "AEROPORTO - ÁREA RESTRITA", poster: "poster-aeroporto.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-14">14</span>` },
-                { horario: "20:00", titulo: "GANHAR OU PERDER", poster: "poster-win.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-livre">L</span>` },
-                { horario: "20:30", titulo: "F1: DIRIGIR PARA VIVER", poster: "poster-f1.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
-            ]
-        },     
-        {
             data: "2025-03-14",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
