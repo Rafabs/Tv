56ad08dd24eea921edd3acc1638b1dca44d94d3a
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2025-03-25 02:45:53 -0300

    Atualizar o poster.js

--- poster.js
+++ poster.js
@@ -7,14 +7,6 @@ function exibirProgramacao() {
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
                                                                                                                 
         {
-            data: "2025-03-24",
-            sessoes: [
-                { horario: "23:59", titulo: "MOANA 2", poster: "poster-moana.jpg", detalhes: `<span class="label-orig">DUB</span> <span class="class-livre">L</span>` },
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "13:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-            ]
-        },     
-        {
             data: "2025-03-26",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
