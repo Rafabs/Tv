264c74e24f2215c777e498a3128e24d77ba35a9c
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2025-04-02 01:09:50 -0300

    MAIN
    Atualização 02.04.25 - Remoção de itens

--- poster.js
+++ poster.js
@@ -5,18 +5,7 @@ function exibirProgramacao() {
     var now = new Date();
     
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
- 
-        {
-            data: "2025-04-01",
-            sessoes: [
-                { horario: "23:59", titulo: "A LISTA DA MINHA VIDA", poster: "poster-lista.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:00", titulo: "ASSASSINATO NA CASA BRANCA", poster: "poster-branca.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
-                { horario: "20:50", titulo: "A CAÇA AO TESOURO DE FENN", poster: "poster-fenn.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },
-            ]
-        },       
+    
         {
             data: "2025-04-02",
             sessoes: [
