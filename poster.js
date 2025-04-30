5bd2f2e91994009403b8aa661fd130eb50933f22
Rafael Barbosa <rafael_barbosa2@hotmail.com>
2025-04-30 00:51:54 -0300

    main
    Atualização 30.04.2025 - Remoção de itens.

--- poster.js
+++ poster.js
@@ -7,17 +7,6 @@ function exibirProgramacao() {
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
                                                                                                                                                     
         {
-            data: "2025-04-29",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "20:00", titulo: "ANDOR", poster: "poster-andor.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
-                { horario: "20:50", titulo: "CONGONHAS: TRAGÉDIA ANUNCIADA", poster: "poster-shadow.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-14">14</span>` },
-                { horario: "21:30", titulo: "COPA DO BRASIL", poster: "poster-copa-do-brasil.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-            ]
-        },      
-        {
             data: "2025-04-30",
             sessoes: [
                 { horario: "23:59", titulo: "YOU", poster: "poster-you.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },
--- script.js
+++ script.js
@@ -10,18 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '29/04') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-orig">DUB</span> <span class="class-12">12</span> <span class="filme">HOW I MET YOUR MOTHER - E06 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">ANDOR - E02 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:50</span> <span class="label-orig">NAC</span> <span class="class-14">14</span> <span class="filme">CONGONHAS: TRAGÉDIA ANUNCIADA - E01 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">21:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">COPA DO BRASIL</span></li> <hr class="filme-separador">    
-`;
-}
-else if (data === '30/04') {
+if (data === '30/04') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li <class="filme-container"> <div class="filme-info"> <span class="horario">00:00</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">YOU - E01 - T05</span> </div> </li> <hr class="filme-separador"> </li>
