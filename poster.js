4e5e0fd2ce7bc0c307aff8937b51a89b077737f1
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2025-05-04 23:38:21 -0300

    Atualizar o poster.js

--- poster.js
+++ poster.js
@@ -7,17 +7,7 @@ function exibirProgramacao() {
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
                                                                                                                                                     
 
-                                                                                                                                          
-        {
-            data: "2025-05-04",
-            sessoes: [
-                { horario: "18:10", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-12">12</span>` },
-                { horario: "18:30", titulo: "AS QUATRO ESTAÇÕES", poster: "poster-estacoes.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
-                { horario: "19:30", titulo: "O ETERNAUTA", poster: "poster-eternauta.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },
-                { horario: "20:30", titulo: "FANTÁSTICO", poster: "poster-fantastico.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "22:00", titulo: "THE LAST OF US", poster: "poster-tlou.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },
-            ]
-        },        
+                                                                                                                                                  
         {
             data: "2025-05-05",
             sessoes: [
