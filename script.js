158b31caa2fbb8465512cece2fd305aee70180e9
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2025-06-01 01:11:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -10,17 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '31/05') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-orig">DUB</span> <span class="class-12">12</span> <span class="filme">HOW I MET YOUR MOTHER - E07 - T03</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">LOVE, DEATH & ROBOTS - E09 - T04</span></li> <hr class="filme-separador">    
-  <li><span class="horario">14:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">CAPITÃO AMÉRICA: ADMIRÁVEL MUNDO NOVO</span></li> <hr class="filme-separador">  
-  <li><span class="horario">18:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador">    
-`;
-}
-else if (data === '01/06') {
+if (data === '01/06') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">19:40</span> <span class="label-orig">DUB</span> <span class="class-12">12</span> <span class="filme">HOW I MET YOUR MOTHER - E08 - T03</span></li> <hr class="filme-separador">    
