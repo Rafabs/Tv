0671dcd872ffdd2da9cd18f42860c4e6a605401b
Rafabs <rafael_barbosa2@hotmail.com>
2025-06-27 23:26:33 -0300

    main
    Atualização 27.06.2025 - Inserção de itens e remoção de datas.

--- script.js
+++ script.js
@@ -10,24 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '25/06') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-orig">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E05 - T04</span></li> <hr class="filme-separador">    
-`;
-}
-else if (data === '27/06') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-orig">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E05 - T04</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">CORAÇÃO DE FERRO - E02 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:55</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">ROUND 6 - E01 - T03</span></li> <hr class="filme-separador">    
-`;
-}
-else if (data === '28/06') {
+if (data === '28/06') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
@@ -117,9 +100,33 @@ else if (data === '04/07') {
 <li class="filme-poster">
 <img src="imgs//poster-brick.jpg" alt="BRICK">
 <div class="filme-texto">
-<span class="dia">11/06</span>
+<span class="dia">10/07</span>
 <span class="class-16">16</span>
 <span class="filme">BRICK</span>
+<div class="filme-texto">
+<span class="class-filme">Filme</span>
+</div>
+</li>
+<hr class="filme-separador">
+
+<li class="filme-poster">
+<img src="imgs//poster-tropicos.jpg" alt="APOCALIPSE NOS TRÓPICOS">
+<div class="filme-texto">
+<span class="dia">14/07</span>
+<span class="class-14">14</span>
+<span class="filme">APOCALIPSE NOS TRÓPICOS</span>
+<div class="filme-texto">
+<span class="class-filme">Filme</span>
+</div>
+</li>
+<hr class="filme-separador">
+
+<li class="filme-poster">
+<img src="imgs//poster-familia.jpg" alt="FAMÍLIA, PERO NO MUCHO">
+<div class="filme-texto">
+<span class="dia">18/07</span>
+<span class="class-10">10</span>
+<span class="filme">FAMÍLIA, PERO NO MUCHO</span>
 <div class="filme-texto">
 <span class="class-filme">Filme</span>
 </div>
