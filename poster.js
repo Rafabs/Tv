4df814bd7e683eb56539c2a18c43a26b81a8020d
Rafabs <rafael_barbosa2@hotmail.com>
2025-07-04 02:10:52 -0300

    main
    Atualização 04.07.2025 - Remoção de datas

--- poster.js
+++ poster.js
@@ -4,19 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-        {
-            data: "2025-07-03",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },                
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
-                { horario: "20:00", titulo: "CORAÇÃO DE FERRO", poster: "poster-ironheart.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-14">14</span>` },                
-                { horario: "20:55", titulo: "TOUR DE FRANCE: NO CORAÇÃO DO PELOTÃO", poster: "poster-tour.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },                
-                { horario: "21:35", titulo: "SANDMAN", poster: "poster-sandman.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },                
-                { horario: "23:59", titulo: "ROUND 6", poster: "poster-round.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` }, 
-            ]
-        },                               
+    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         {
             data: "2025-07-04",
             sessoes: [
