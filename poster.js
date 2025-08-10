a8aef2b510acac73088f1cdc7c81f82f5643b981
Rafabs <rafael_barbosa2@hotmail.com>
2025-08-09 22:47:46 -0300

    main
    Atualização 09.08 - Remoção de datas

--- poster.js
+++ poster.js
@@ -4,16 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-        {
-            data: "2025-08-09",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-sp1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:10", titulo: "SP2", poster: "poster-sp2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
-                { horario: "20:00", titulo: "WANDINHA", poster: "poster-wandinha.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },                           
-            ]
-        },     
+    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
         {
             data: "2025-08-10",
             sessoes: [
