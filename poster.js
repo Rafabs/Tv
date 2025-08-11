6628ea1c1e8b7d3e30b1afbb3bd72c14dc244eef
Rafabs <rafael_barbosa2@hotmail.com>
2025-08-10 22:59:51 -0300

    main
    Atualização 10.08 - Remoção de datas

--- poster.js
+++ poster.js
@@ -6,13 +6,29 @@ function exibirProgramacao() {
     
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
         {
-            data: "2025-08-10",
+            data: "2025-08-11",
             sessoes: [
-                { horario: "19:05", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
-                { horario: "19:25", titulo: "WANDINHA", poster: "poster-wandinha.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },                           
-                { horario: "20:30", titulo: "FANTÁSTICO", poster: "poster-fantastico.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "13:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
             ]
-        },                                                                                                            
+        },          
+        {
+            data: "2025-08-13",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "18:45", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:15", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
+                { horario: "19:35", titulo: "WANDINHA", poster: "poster-wandinha.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },                           
+            ]
+        },   
+        {
+            data: "2025-08-14",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },                
+            ]
+        }, 
     ];
 
     var dataAtual = now.toISOString().split('T')[0];
