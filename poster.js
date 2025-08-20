8fd97ab4ca9d94447e1a1ea95a53467e0aba6d82
Rafabs <rafael_barbosa2@hotmail.com>
2025-08-20 00:20:20 -0300

    main
    Atualização 20.08.2025 - Remoção de data

--- poster.js
+++ poster.js
@@ -5,14 +5,6 @@ function exibirProgramacao() {
     var now = new Date();
     
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            {   
-            data: "2025-08-19",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },   
-            ]
-        },       
-        {
             data: "2025-08-20",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
