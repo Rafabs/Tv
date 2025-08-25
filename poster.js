6ec7500f20f5a4663be1f5267120dc93b979610b
Rafabs <rafael_barbosa2@hotmail.com>
2025-08-25 00:35:04 -0300

    main
    Atualização 25.08 - Inserção de itens e remoção de datas

--- poster.js
+++ poster.js
@@ -6,22 +6,32 @@ function exibirProgramacao() {
     
     var diasProgramacao = [  
     {   
-            data: "2025-08-23",
+            data: "2025-08-25",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
                 { horario: "13:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },   
-                { horario: "13:45", titulo: "LONG STORY SHORT", poster: "poster-long.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },   
-                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
             ]
-        },     
-        {
-            data: "2025-08-24",
+        },   
+    {   
+            data: "2025-08-27",
             sessoes: [
-                { horario: "19:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },   
-                { horario: "19:45", titulo: "LONG STORY SHORT", poster: "poster-long.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },   
-                { horario: "20:10", titulo: "CAMPEONATO BRASILEIRO", poster: "poster-campeonato-brasileiro.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "18:45", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:15", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },   
+                { horario: "19:35", titulo: "LONG STORY SHORT", poster: "poster-long.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },   
+                { horario: "23:59", titulo: "PECADORES", poster: "poster-pecadores.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },   
             ]
-        },                                               
+        },        
+    {   
+            data: "2025-08-28",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },   
+                { horario: "20:00", titulo: "LONG STORY SHORT", poster: "poster-long.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },   
+                { horario: "23:59", titulo: "O LOBO DE WALL STREET", poster: "poster-lobo.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },   
+            ]
+        },                                                               
     ];
 
     var dataAtual = now.toISOString().split('T')[0];
--- script.js
+++ script.js
@@ -10,24 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '23/08') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E18 - T05</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:45</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E02 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-`;
-}
-else if (data === '24/08') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">19:25</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E19 - T05</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:45</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E03 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">CAMPEONATO BRASILEIRO</span></li> <hr class="filme-separador"> 
-`;
-}
-else if (data === '25/08') {
+if (data === '25/08') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
@@ -41,6 +24,7 @@ else if (data === '27/08') {
   <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
   <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E21 - T05</span></li> <hr class="filme-separador">    
   <li><span class="horario">19:35</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E04 - T01</span></li> <hr class="filme-separador">    
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">PECADORES</span> </div> </li> <hr class="filme-separador"> </li>
   `;
 }
 else if (data === '28/08') {
@@ -50,6 +34,7 @@ else if (data === '28/08') {
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E22 - T05</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E05 - T01</span></li> <hr class="filme-separador">    
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O LOBO DE WALL STREET</span> </div> </li> <hr class="filme-separador"> </li>
   `;
 }
 else if (data === '29/08') {
@@ -66,6 +51,7 @@ else if (data === '01/09') {
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E24 - T05</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E06 - T01</span></li> <hr class="filme-separador">    
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O BRUTALISTA</span> </div> </li> <hr class="filme-separador"> </li>
   `;
 }
 else if (data === '02/09') {
@@ -75,6 +61,7 @@ else if (data === '02/09') {
   <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E01 - T06</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E07 - T01</span></li> <hr class="filme-separador">    
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">JOGOS VORAZES: A CANTIGA DOS PÁSSAROS E DAS SERPENTES</span> </div> </li> <hr class="filme-separador"> </li>
   `;
 }
 else if (data === '03/09') {
@@ -92,6 +79,7 @@ else if (data === '05/09') {
   <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E01 - T06</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">LONG STORY SHORT - E07 - T01</span></li> <hr class="filme-separador">    
   <li><span class="horario">20:25</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">WANDINHA - E05 - T02</span></li> <hr class="filme-separador">      
+  <li <class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">LOOPER: ASSASSINOS DO FUTURO</span> </div> </li> <hr class="filme-separador"> </li>
   `;
 }
 else if (data === '06/09') {
