50edcf536721e1a09a2a0989d55a43bc85680258
Rafabs <rafael_barbosa2@hotmail.com>
2025-10-06 23:46:00 -0300

    main
    Atualização 06.10 - Remoção de data

--- poster.js
+++ poster.js
@@ -6,17 +6,6 @@ function exibirProgramacao() {
     
     var diasProgramacao = [                                      
     {   
-            data: "2025-10-06",
-            sessoes: [
-                { horario: "23:59", titulo: "ALICE IN BORDERLAND", poster: "poster-alice.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },   
-                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
-                { horario: "20:00", titulo: "HOUSE OF GUINNESS", poster: "poster-house.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },                   
-                { horario: "20:50", titulo: "HOTEL ASSOMBRADO", poster: "poster-hotel.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },   
-            ]
-        },    
-    {   
             data: "2025-10-07",
             sessoes: [
                 { horario: "23:59", titulo: "ALICE IN BORDERLAND", poster: "poster-alice.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },   
