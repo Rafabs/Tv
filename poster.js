9845bf9fcb3758f233929d4130df38d4fa093ddf
Rafabs <rafael_barbosa2@hotmail.com>
2025-10-12 12:16:48 -0300

    main
    Atualização 11.10 - Remoção de data e itens

--- poster.js
+++ poster.js
@@ -4,17 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                               
-    {   
-            data: "2025-10-11",
-            sessoes: [
-                { horario: "23:59", titulo: "LIMPA", poster: "poster-limpa.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },   
-                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "13:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
-                { horario: "13:45", titulo: "DESOBEDIENTES", poster: "poster-desobedientes.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },                   
-                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-            ]
-        },       
+    var diasProgramacao = [                                                  
     {   
             data: "2025-10-12",
             sessoes: [
