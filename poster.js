1f90480ae44966e84f371fa0ee7abb35a9c12aa6
Rafabs <rafael_barbosa2@hotmail.com>
2025-10-25 01:54:09 -0300

    main

--- poster.js
+++ poster.js
@@ -6,14 +6,23 @@ function exibirProgramacao() {
     
     var diasProgramacao = [                                                                                                                                                                                                                                                                  
     {   
-            data: "2025-10-24",
+            data: "2025-10-25",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
                 { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
                 { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
                 { horario: "20:00", titulo: "SPLINTER CELL: DEATHEATCH", poster: "poster-splinter.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-16">16</span>` },                   
+                { horario: "21:00", titulo: "CAMPEONATO BRASILEIRO", poster: "poster-campeonato-brasileiro.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
             ]
-        },                 
+        },         
+    {   
+            data: "2025-10-27",
+            sessoes: [
+                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
+                { horario: "19:40", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
+            ]
+        },                   
     ];
 
     var dataAtual = now.toISOString().split('T')[0];
