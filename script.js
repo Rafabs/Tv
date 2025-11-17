2182e70e8b6481145f5b44ba16d1ab963a34b532
Rafabs <rafael_barbosa2@hotmail.com>
2025-11-17 00:44:51 -0300

    msin

--- script.js
+++ script.js
@@ -10,15 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '16/11') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">19:05</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E08 - T08</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:30</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">O MONSTRO EM MIM - E03 - T01</span></li> <hr class="filme-separador">      
-  <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador"> 
-  `;
-}
-else if (data === '17/11') {
+if (data === '17/11') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">      
@@ -136,6 +128,19 @@ else if (data === '29/11') {
 <span class="filme">THE BEATLES ANTHOLOGY</span>
 <div class="filme-texto">
 <span class="class-documentario">Documentário</span>
+</div>
+</li>
+<hr class="filme-separador">
+
+<li class="filme-poster">
+<img src="imgs//poster-abandonados.jpg" alt="OS ABANDONADOS">
+<div class="filme-texto">
+<span class="dia">04/12</span>
+<span class="class-18">18</span>
+<span class="filme">OS ABANDONADOS</span>
+<div class="filme-texto">
+<span class="class-serie">Série</span>
+<span class="class-temporada">T1</span>
 </div>
 </li>
 <hr class="filme-separador">
