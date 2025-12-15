9760e9fd25b886fd6d1e940ed88314015ea1ebc1
Rafabs <rafael_barbosa2@hotmail.com>
2025-12-15 00:01:20 -0300

    main

--- poster.js
+++ poster.js
@@ -4,25 +4,7 @@ function exibirProgramacao() {
     var programacaoDiv = document.getElementById("programacao");
     var now = new Date();
     
-    var diasProgramacao = [                                                                                                                                                                                                                                                                                                         
-    {   
-            data: "2025-12-13",
-            sessoes: [
-                { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-                { horario: "13:25", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
-                { horario: "13:45", titulo: "OS ABANDONADOS", poster: "poster-abandonados.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },  
-                { horario: "14:45", titulo: "PERCY JACKSON E OS OLÍMPIANOS", poster: "poster-percy.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-12">12</span>` },  
-                { horario: "19:10", titulo: "SP2", poster: "poster-SP2.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-            ]
-        },        
-    {   
-            data: "2025-12-14",
-            sessoes: [
-                { horario: "19:20", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
-                { horario: "19:40", titulo: "OS ABANDONADOS", poster: "poster-abandonados.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-18">18</span>` },  
-                { horario: "20:30", titulo: "FANTASTICO", poster: "poster-fantastico.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
-            ]
-        },           
+    var diasProgramacao = [                                                                                                                                                                                                                                                                                                                  
     {   
             data: "2025-12-15",
             sessoes: [
--- script.js
+++ script.js
@@ -10,25 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '13/12') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E03 - T09</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:45</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">OS ABANDONADOS - E05 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">14:45</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">PERCY JACKSON E OS OLÍMPIANOS - E02 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  `;
-}
-else if (data === '14/12') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">19:20</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">HOW I MET YOUR MOTHER - E04 - T09</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:40</span> <span class="label-leg">LEG</span> <span class="class-18">18</span> <span class="filme">OS ABANDONADOS - E06 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador"> 
-  `;
-}
-else if (data === '15/12') {
+if (data === '15/12') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador">    
