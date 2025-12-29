d5583052e1642ce6bb75b3d3f2ca7c8e20eecdeb
Rafabs <rafael_barbosa2@hotmail.com>
2025-12-28 23:21:26 -0300

    main

--- poster.js
+++ poster.js
@@ -6,14 +6,6 @@ function exibirProgramacao() {
     
     var diasProgramacao = [                                                                                                                                                                                                                                                                                                                  
     {   
-            data: "2025-12-28",
-            sessoes: [
-                { horario: "18:55", titulo: "HOW I MET YOUR MOTHER", poster: "poster-himym.jpg", detalhes: `<span class="label-dub">DUB</span> <span class="class-14">14</span>` },  
-                { horario: "19:15", titulo: "STRANGER THINGS", poster: "poster-things.jpg", detalhes: `<span class="label-leg">LEG</span> <span class="class-16">16</span>` },  
-                { horario: "20:30", titulo: "FANTÁSTICO", poster: "poster-fantastico.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },                                                                                  
-            ]
-        },     
-    {   
             data: "2025-12-29",
             sessoes: [
                 { horario: "11:45", titulo: "SP1", poster: "poster-SP1.jpg", detalhes: `<span class="label-orig">NAC</span> <span class="class-12">12</span>` },        
