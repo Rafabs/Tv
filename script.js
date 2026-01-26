b31c966c1ba46a81e595f2acdc706055f400c777
Rafabs <rafael_barbosa2@hotmail.com>
2026-01-25 23:23:49 -0300

    main

--- script.js
+++ script.js
@@ -10,24 +10,7 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '24/01') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">13:25</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">MODERN FAMILY - E07 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">13:50</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">PERCY JACKSON E OS OLIMPIANOS - E08 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">18:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">PAULISTÃO</span></li> <hr class="filme-separador"> 
-  `;
-}
-else if (data === '25/01') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">20:10</span> <span class="label-dub">DUB</span> <span class="class-12">12</span> <span class="filme">MODERN FAMILY - E08 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:30</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">FANTÁSTICO</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">23:00</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <span class="filme">O CAVALEIRO DOS SETE REINOS - E02 - T01</span></li> <hr class="filme-separador">    
-  `;
-}
-else if (data === '26/01') {
+if (data === '26/01') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
