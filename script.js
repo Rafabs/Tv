d3d4ad11a64824fd9f8059156e9cc465b3b3fe75
Rafabs <rafael_barbosa2@hotmail.com>
2026-02-16 16:26:19 -0300

    MAIN

--- script.js
+++ script.js
@@ -10,22 +10,15 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '13/02') {
+if (data === '16/02') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E22 - T01</span></li> <hr class="filme-separador">    
-  `;
-}
-else if (data === '16/02') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E23 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">FILHOS DO CHUMBO - E03 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">DE BELFAST AO PARAÍSO - E02 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">22:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E02 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">19:15</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E23 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:35</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">FILHOS DO CHUMBO - E03 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">20:25</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">DE BELFAST AO PARAÍSO - E02 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">21:25</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E02 - T01</span></li> <hr class="filme-separador"> 
   <li><class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-16">16</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">O CAVALEIRO DOS SETE REINOS - EP.05 - T01</span> </div> </li> <hr class="filme-separador"> </li>  
   `;
 }
@@ -33,10 +26,10 @@ else if (data === '17/02') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E24 - T01</span></li> <hr class="filme-separador">    
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">FILHOS DO CHUMBO - E04 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">DE BELFAST AO PARAÍSO - E03 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">19:05</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">19:25</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E24 - T01</span></li> <hr class="filme-separador">    
+  <li><span class="horario">19:45</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">FILHOS DO CHUMBO - E04 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">20:45</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">DE BELFAST AO PARAÍSO - E03 - T01</span></li> <hr class="filme-separador"> 
   <li><class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E03 - T01</span> </div> </li> <hr class="filme-separador"> </li>  
   `;
 }
