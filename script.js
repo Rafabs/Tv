2df3da08742908c633badc067f02a0a22ee9fbc0
Rafabs <rafael_barbosa2@hotmail.com>
2026-03-02 22:54:13 -0300

    main

--- script.js
+++ script.js
@@ -10,33 +10,14 @@ function mostrarProgramacao(button, data) {
   // Adiciona a classe 'active' apenas ao botão clicado
   button.classList.add("active");
 
-if (data === '27/02') {
+if (data === '03/03') {
   programacaoDiv.innerHTML = `
   <ul>  
   <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E08 - T02</span></li> <hr class="filme-separador">    
-  `;
-}
-else if (data === '02/03') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:10</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E09 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:30</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">STRIP LAW - E04 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-12">12</span> <span class="filme">DE BELFAST AO PARAÍSO - E08 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">21:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E07 - T01</span></li> <hr class="filme-separador">    
-  <li class="filme-container"> <div class="filme-info"> <span class="horario">23:59</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <img src="imgs//dts.png" alt="Logo DTS" class="logo-dts"> <span class="filme">SÃO PAULO SOCIEDADE ANÔNIMA</span> </div> </li> <hr class="filme-separador"> </li>  
-  `;
-}
-else if (data === '03/03') {
-  programacaoDiv.innerHTML = `
-  <ul>  
-  <li><span class="horario">11:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP1</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">18:45</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">19:10</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E10 - T02</span></li> <hr class="filme-separador">    
-  <li><span class="horario">19:30</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">STRIP LAW - E05 - T01</span></li> <hr class="filme-separador"> 
-  <li><span class="horario">20:00</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E08 - T01</span></li> <hr class="filme-separador">      
+  <li><span class="horario">19:10</span> <span class="label-orig">NAC</span> <span class="class-12">12</span> <span class="filme">SP2</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">19:40</span> <span class="label-dub">DUB</span> <span class="class-14">14</span> <span class="filme">MODERN FAMILY - E10 - T02</span></li> <hr class="filme-separador">    
+  <li><span class="horario">20:00</span> <span class="label-dub">DUB</span> <span class="class-16">16</span> <span class="filme">STRIP LAW - E05 - T01</span></li> <hr class="filme-separador"> 
+  <li><span class="horario">20:30</span> <span class="label-leg">LEG</span> <span class="class-14">14</span> <span class="filme">MISTÉRIO DE UM MILHÃO DE SEGUIDORES - E08 - T01</span></li> <hr class="filme-separador">      
   `;
 }
 else if (data === '04/03') {
