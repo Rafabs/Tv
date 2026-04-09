8fd9dd424090660273f61a63767686e1bfae8986
Rafabs <rafael_barbosa2@hotmail.com>
2026-04-09 02:05:04 -0300

    main

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://Rafabs.github.io',
+  base: '/Tv', // Nome do seu repositório exatamente como está no GitHub
+});
